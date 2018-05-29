c95b1465b7cbfc57cd1e858a42f622f9dda54f31
wuweijia1994 <wuweijia1994@gmail.com>
2018-05-29 10:00:47 -0700

    used with camera

--- egg.py
+++ egg.py
@@ -3,7 +3,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 # Create a VideoCapture object
-cap = cv2.VideoCapture('egg.mp4')
+cap = cv2.VideoCapture(1)
 
 # Check if camera opened successfully
 if (cap.isOpened() == False):
@@ -26,22 +26,23 @@ while(True):
         # Display the resulting frame
         thresh = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel)
         im2,contours,hierarchy = cv2.findContours(thresh, 1, 2)
-        cnt = contours[0]
-        M = cv2.moments(cnt)
-        #print( M )
-        cx = int(M['m10']/M['m00'])
-        cy = int(M['m01']/M['m00'])
-        print( cx,cy )
-        thresh=cv2.cvtColor(thresh,cv2.COLOR_GRAY2RGB)
-        cv2.circle(thresh,(cx,cy), 12, (0,0,255), -1)
-        cv2.drawContours(thresh, contours, 0, (255,0,255), 3)
-        #cv2.waitKey(3)
-        cv2.imshow('eggs',thresh)
-        # Write the frame into the file 'output.avi'
-        out.write(thresh)
-        # Press Q on keyboard to stop recording
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+        if(len(contours) > 0):
+            cnt = contours[0]
+            M = cv2.moments(cnt)
+            #print( M )
+            cx = int(M['m10']/M['m00'])
+            cy = int(M['m01']/M['m00'])
+            print( cx,cy )
+            thresh=cv2.cvtColor(thresh,cv2.COLOR_GRAY2RGB)
+            cv2.circle(thresh,(cx,cy), 12, (0,0,255), -1)
+            cv2.drawContours(thresh, contours, 0, (255,0,255), 3)
+            #cv2.waitKey(3)
+            cv2.imshow('eggs',thresh)
+            # Write the frame into the file 'output.avi'
+            out.write(thresh)
+            # Press Q on keyboard to stop recording
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
     # Break the loop
     else:
         break
