0a3bb7a59b3fb444397b3e4c473ae25e8130cf71
wuweijia1994 <wuweijia1994@gmail.com>
2018-06-04 19:41:49 -0700

    prepare to add multiple eggs

--- RobotArmPickEgg.py
+++ RobotArmPickEgg.py
@@ -1,91 +1,121 @@
 import sys, os
-# sys.path.remove('/opt/ros/kinetic/lib/python2.7/dist-packages/')
+sys.path.remove('/opt/ros/kinetic/lib/python2.7/dist-packages')
 from time import sleep
 import egg
 # import cv2
+import SwiftBluetooth
 
 import time
 import serial
 
-# configure the serial connections (the parameters differs on the device you are connecting to)
-ser = serial.Serial(
-    port='/dev/ttyUSB0',
-    baudrate=115200
-    # parity=serial.PARITY_ODD,
-    # stopbits=serial.STOPBITS_TWO,
-    # bytesize=serial.SEVENBITS
-)
+if __name__ == "__main__":
+    arm = SwiftBluetooth.SwiftRobotArm()
+    # arm.attachMotors()
+    arm.resetArm()
 
-ser.isOpen()
-# ser.write('#n G0 X100 Y100 Z200 F1000\n')
+    print("Put the egg in a proper position.")
+    while(1):
+        move_on = input("When you are done, press d. ")
+        if(move_on == "d"):
+            print("Egg detection is done.")
+            break
+    cx, cy = egg.eggPositionByCamera(1)
+    ax, ay = egg.xyMapping(cx, cy)
+    print(ax, ay)
+    arm.move(ax, ay)
+    arm.move(ax, ay, 100)
+    arm.pumpWorking()
+    time.sleep(2)
+    arm.move(ax, ay, 150)
+    time.sleep(5)
 
-ser.write('#n G0 X100 Y100 Z100 F1000\n')
-time.sleep(8)
+    arm.move(ax, ay, 100)
+    time.sleep(2)
+    arm.pumpStop()
+    arm.resetArm()
+    while 1:
+        pass
 
-image = egg.getImage(1)
-image_x, image_y = egg.getEggPosition(image)
-arm_x, arm_y = egg.xyMapping(image_x, image_y)
 
-print(arm_x, arm_y)
-# ser.write()
-cmd = '#n G0 X' + str(arm_x) + ' Y' + str(arm_y) +' Z150 F1000\n'
-print(cmd)
-print('#n G0 X100 Y100 Z100 F1000\n')
-ser.write(cmd)
-time.sleep(8)
-cmd = '#n G0 X' + str(arm_x) + ' Y' + str(arm_y) +' Z100 F1000\n'
-ser.write(cmd)
-
-time.sleep(8)
-
-cmd = '#n M2231 V1\n'
-ser.write(cmd)
-
-time.sleep(3)
-
-cmd = '#n M2231 V0\n'
-ser.write(cmd)
-
-time.sleep(3)
-
+# # configure the serial connections (the parameters differs on the device you are connecting to)
+# ser = serial.Serial(
+#     port='/dev/ttyUSB0',
+#     baudrate=115200
+#     # parity=serial.PARITY_ODD,
+#     # stopbits=serial.STOPBITS_TWO,
+#     # bytesize=serial.SEVENBITS
+# )
+#
+# ser.isOpen()
+# # ser.write('#n G0 X100 Y100 Z200 F1000\n')
+#
+# ser.write('#n G0 X100 Y100 Z100 F1000\n')
+# time.sleep(8)
+#
+# image = egg.getImage(1)
+# image_x, image_y = egg.getEggPosition(image)
+# arm_x, arm_y = egg.xyMapping(image_x, image_y)
+#
+# print(arm_x, arm_y)
+# # ser.write()
+# cmd = '#n G0 X' + str(arm_x) + ' Y' + str(arm_y) +' Z150 F1000\n'
+# print(cmd)
+# print('#n G0 X100 Y100 Z100 F1000\n')
+# ser.write(cmd)
+# time.sleep(8)
 # cmd = '#n G0 X' + str(arm_x) + ' Y' + str(arm_y) +' Z100 F1000\n'
 # ser.write(cmd)
-
+#
 # time.sleep(8)
-# while True:
-#     image = egg.getImage(1)
-#     image_x, image_y = egg.getEggPosition(image)
-#     arm_x, arm_y = egg.xyMapping(image_x, image_y)
 #
-#     print(arm_x, arm_y)
-#     # ser.write()
-#     cmd = '#n G0 X' + str(arm_x) + ' Y' + str(arm_y) +' Z150 F1000\n'
-#     print(cmd)
-#     print('#n G0 X100 Y100 Z100 F1000\n')
-#     ser.write(cmd)
-#     time.sleep(5)
-
-# sys.path.append(os.path.join(os.path.dirname(__file__), '../..'))
-
-
-# from uf.wrapper.swift_api import SwiftAPI
-# from uf.utils.log import *
+# cmd = '#n M2231 V1\n'
+# ser.write(cmd)
 #
-# #logger_init(logging.VERBOSE)
-# #logger_init(logging.DEBUG)
-# logger_init(logging.INFO)
+# time.sleep(3)
 #
-# print('setup swift ...')
+# cmd = '#n M2231 V0\n'
+# ser.write(cmd)
 #
-# #swift = SwiftAPI(dev_port = '/dev/ttyACM0')
-# #swift = SwiftAPI(filters = {'hwid': 'USB VID:PID=2341:0042'})
-# swift = SwiftAPI() # default by filters: {'hwid': 'USB VID:PID=2341:0042'}
+# time.sleep(3)
+#
+# # cmd = '#n G0 X' + str(arm_x) + ' Y' + str(arm_y) +' Z100 F1000\n'
+# # ser.write(cmd)
+#
+# # time.sleep(8)
+# # while True:
+# #     image = egg.getImage(1)
+# #     image_x, image_y = egg.getEggPosition(image)
+# #     arm_x, arm_y = egg.xyMapping(image_x, image_y)
+# #
+# #     print(arm_x, arm_y)
+# #     # ser.write()
+# #     cmd = '#n G0 X' + str(arm_x) + ' Y' + str(arm_y) +' Z150 F1000\n'
+# #     print(cmd)
+# #     print('#n G0 X100 Y100 Z100 F1000\n')
+# #     ser.write(cmd)
+# #     time.sleep(5)
+#
+# # sys.path.append(os.path.join(os.path.dirname(__file__), '../..'))
 #
 #
-# print('sleep 2 sec ...')
-# sleep(2)
-#
-# swift.set_position(10, 0, 0, speed = 1500, timeout = 20)
-#
-# while True:
-#     sleep(1)
+# # from uf.wrapper.swift_api import SwiftAPI
+# # from uf.utils.log import *
+# #
+# # #logger_init(logging.VERBOSE)
+# # #logger_init(logging.DEBUG)
+# # logger_init(logging.INFO)
+# #
+# # print('setup swift ...')
+# #
+# # #swift = SwiftAPI(dev_port = '/dev/ttyACM0')
+# # #swift = SwiftAPI(filters = {'hwid': 'USB VID:PID=2341:0042'})
+# # swift = SwiftAPI() # default by filters: {'hwid': 'USB VID:PID=2341:0042'}
+# #
+# #
+# # print('sleep 2 sec ...')
+# # sleep(2)
+# #
+# # swift.set_position(10, 0, 0, speed = 1500, timeout = 20)
+# #
+# # while True:
+# #     sleep(1)
--- egg.py
+++ egg.py
@@ -1,65 +1,26 @@
 import cv2
-
+import time
 
 def getEggPosition(image):
     import numpy as np
     from matplotlib import pyplot as plt
-
-    # Create a VideoCapture object
-    # cap = cv2.VideoCapture(1)
-    # cap = cv2.imread(image, cv2.IMREAD_COLOR)
-
-    # Check if camera opened successfully
-    # if (cap.isOpened() == False):
-    #     print("Unable to read camera feed")
-
-    # Default resolutions of the frame are obtained.The default resolutions are system dependent.
-    # We convert the resolutions from float to integer.
-    # frame_width = int(cap.get(3))
-    # frame_height = int(cap.get(4))
     kernel = cv2.getStructuringElement(cv2.MORPH_RECT,(8, 8))
-    # Define the codec and create VideoWriter object.The output is stored in 'outpy.avi' file.
-    # fourcc = cv2.VideoWriter_fourcc(*'XVID')
-    # out = cv2.VideoWriter('eggContour.avi',fourcc, 10, (frame_width,frame_height))
-
-    # while(True):
-    #     ret, frame = cap.read()
-    #     if ret == True:
     GrayImage=cv2.cvtColor(image,cv2.COLOR_RGB2GRAY)
     ret,thresh=cv2.threshold(GrayImage,230,255,cv2.THRESH_BINARY)
+
     # Display the resulting frame
     thresh = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel)
     im2,contours,hierarchy = cv2.findContours(thresh, 1, 2)
     if(len(contours) > 0):
         cnt = contours[0]
         M = cv2.moments(cnt)
-        #print( M )
         cx = int(M['m10']/M['m00'])
         cy = int(M['m01']/M['m00'])
-        # print( cx,cy )
         return cx, cy
-        # thresh=cv2.cvtColor(thresh,cv2.COLOR_GRAY2RGB)
-        # cv2.circle(thresh,(cx,cy), 12, (0,0,255), -1)
-        # cv2.drawContours(thresh, contours, 0, (255,0,255), 3)
-        #cv2.waitKey(3)
-        # cv2.imshow('eggs',thresh)
-        # Write the frame into the file 'output.avi'
-        # out.write(thresh)
-        # Press Q on keyboard to stop recording
-        # if cv2.waitKey(1) & 0xFF == ord('q'):
-        #     break
-        # Break the loop
-        # else:
-        #     break
     else:
         print("Not Found!")
+        return None, None
 
-    # When everything done, release the video capture and video write objects
-    # cap.release()
-    # out.release()
-
-    # Closes all the frames
-    # cv2.destroyAllWindows()
 
 def getImage(camera):
     cap = cv2.VideoCapture(1)
@@ -70,7 +31,16 @@ def getImage(camera):
     cap.release()
     return frame
 
+def eggPositionByCamera(camera):
+    while(1):
+        frame = getImage(camera)
+        x, y = getEggPosition(frame)
+        if(x == None):
+            print("Did not detect any egg. Please try again")
+        else:
+            return x, y
+        time.sleep(2)
+
 def xyMapping(x, y):
-    x_offset = 380
-    y_offset = 120
-    return int((y-y_offset)/1.05), int((x-x_offset)/1.31)
+
+    return int(y * 0.45893878 + 42.01635299), int(x * 0.50576957 -173)
